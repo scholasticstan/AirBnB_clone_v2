473c0ed19a17ab0b7a72c4525b985b805829f9b9
Ocholi Joseph <ocholijoseph@gmail.com>
2023-05-16 18:23:39 -0700

    Commited the whole SQL task

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,8 @@
+-- SQL script to display the maximum temperature of each state, ordered by state name
+
+USE hbtn_0c_0;
+
+SELECT state, MAX(temperature) AS max_temperature
+FROM weather
+GROUP BY state
+ORDER BY state;
