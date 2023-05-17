286d9c6206a2142a528d14a8d36ed0c2a3b48d4c
Ocholi Joseph <ocholijoseph@gmail.com>
2023-05-16 17:36:00 -0700

    New page to push

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,7 +1,9 @@
 #!/bin/bash
--- This script list all data in a table
+-- Script to list all rows of the table first_table from the database hbtn_0c_0
 
+-- Get the database name from the command line argument
 database=$1
 
+-- Run the SQL query using the mysql command
 mysql -uroot -p -e "USE $database; SELECT * FROM first_table;"
 
