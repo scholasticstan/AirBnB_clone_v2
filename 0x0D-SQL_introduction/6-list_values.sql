3ab8706f3c43ada8a2428c7080d50e48145f7187
Ocholi Joseph <ocholijoseph@gmail.com>
2023-05-16 17:44:55 -0700

    first_table Task

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,8 +1,3 @@
-#!/bin/bash
 -- Script to list all rows of the table first_table from the database hbtn_0c_0
-
--- Get the database name from the command line argument
-database=$1
-
--- Run the SQL query using the mysql command
-echo "SELECT * FROM first_table;" | mysql -h localhost -u root -p $database
+SELECT * 
+from first_table;
