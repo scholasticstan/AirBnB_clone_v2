4949ea01a4e59b88fbfd7f6bfc96d4c15eee6a2c
Ocholi Joseph <ocholijoseph@gmail.com>
2023-05-16 18:18:12 -0700

    Task 101

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,9 @@
+-- SQL script to display the average temperature (Fahrenheit) by city ordered by temperature (descending)
+
+USE hbtn_0c_0;
+
+SELECT city, AVG(temperature) AS avg_temperature
+FROM weather
+GROUP BY city
+ORDER BY avg_temperature DESC;
+
